7da57588308e50b3f438a97ae43467fc8fa7b9e4
AlostXD <junio.ribeiro02@hotmail.com>
2024-10-30 23:48:49 -0300

    Refeito os arquivos bases (Shortcut Icon ainda não funciona) e feito também a homepage com a Navbar

---
+++ vortex/src/app/bate-ponto/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function BatePonto() {
+    return (
+        <div>
+            <h1>Bate Ponto</h1>
+        </div>
+    )
+}
--- vortex/src/app/components/navbar.tsx
+++ vortex/src/app/components/navbar.tsx
@@ -3,12 +3,25 @@ import Image from "next/image";
 export default function Navbar() {
     return (
         <>
-            <nav className="bg-black">
-                <div>
-                    <Image src="/favicon.ico" alt="Vortex" width={100} height={100} />
-                    <h1>Vortex</h1>
-                </div>
+        <div className="border-b-2 border-b-white w-full bg-neutral-900">
+            <nav className="flex flex-col items-center gap-3 max-w-screen-lg m-auto p-2 md:flex-row md:justify-between">
+                <a className="flex items-center justify-center gap-4" href="./bate-ponto">
+                    <Image src="/vortex.svg" alt="Vortex" width={50} height={50} />
+                    <h1 className="italic font-extrabold text-xl">Vortex</h1>
+                </a>
+                <ul className="flex flex-col items-center gap-12 font-bold sm:flex-row sm:justify-between">
+                    <li className="transition duration-500 ease-in-out hover:scale-x-105 hover:text-gray-700">
+                        <a href="#">Home</a>
+                    </li >
+                    <li className="transition duration-500 ease-in-out hover:scale-x-105 hover:text-gray-700">
+                        <a href="#">Bate-Ponto</a>
+                    </li>
+                    <li className="transition duration-500 ease-in-out hover:scale-x-105 hover:text-gray-700">
+                        <a href="#">Administrativo</a>
+                    </li>
+                </ul>
             </nav>
+        </div>
         </>
     )
 }
