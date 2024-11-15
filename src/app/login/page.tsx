da06a9fb77c91ab86bf2d5263cbf4d27536864dc
AlostXD <junio.ribeiro02@hotmail.com>
2024-11-15 10:03:48 -0300

    Reiniciado o progeto com a API funcionando juntamente com a Login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import SignInButton from "@/components/signin-button";
+import React from "react";
+
+type Props = {};
+
+export default function Page({}: Props) {
+  return (
+    <>
+        <div className=""></div>
+      <SignInButton />
+    </>
+  );
+}
---
+++ src/components/userAvatar.tsx
@@ -0,0 +1,17 @@
+import { auth } from "../auth";
+import Image from "next/image";
+
+export default async function UserAvatar() {
+  const session = await auth();
+
+  if (!session?.user) return null;
+
+  return (
+    <div>
+      <Image
+        src={session.user.image || "/default-avatar.png"}
+        alt="User Avatar"
+      />
+    </div>
+  );
+}
