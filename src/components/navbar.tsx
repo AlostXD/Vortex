58c8569f0def44bca10933cdf46c509999eed4fe
AlostXD <junio.ribeiro02@hotmail.com>
2024-11-18 22:22:06 -0300

    Deploy vercel bugs3

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,25 +6,34 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-    <div className="border-b-2 borber-b-black dark:border-b-white w-full bg-white dark:bg-neutral-900 text-black dark:text-white">
+      <div className="border-b-2 borber-b-black dark:border-b-white w-full bg-white dark:bg-neutral-900 text-black dark:text-white">
         <nav className="flex flex-col items-center gap-3 max-w-screen-lg m-auto p-2 md:flex-row md:justify-between">
-            <Link className="flex items-center justify-center gap-4" href="./">
-                <Image src={logo} alt="Vortex" width={50} height={50} />
-                <h1 className="italic font-extrabold text-xl">Vortex</h1>
+          <Link className="flex items-center justify-center gap-4" href="./">
+            <Image src={logo} alt="Vortex" width={50} height={50} />
+            <h1 className="italic font-extrabold text-xl">Vortex</h1>
+          </Link>
+          <ul className="flex flex-col items-center gap-12 font-bold sm:flex-row sm:justify-between">
+            <Link
+              className="transition ease-in-out hover:scale-x-105 hover:text-gray-700"
+              href="/"
+            >
+              Home
             </Link>
-            <ul className="flex flex-col items-center gap-12 font-bold sm:flex-row sm:justify-between">
-                <Link className="transition ease-in-out hover:scale-x-105 hover:text-gray-700" href="/">
-                    <a>Home</a>
-                </Link>
-                <Link className="transition ease-in-out hover:scale-x-105 hover:text-gray-700" href="/bateponto">
-                    <a>Bate-Ponto</a>
-                </Link>
-                <Link className="transition ease-in-out hover:scale-x-105 hover:text-gray-700" href="/administrativo">
-                    <a>Administrativo</a>
-                </Link>
-            </ul>
+            <Link
+              className="transition ease-in-out hover:scale-x-105 hover:text-gray-700"
+              href="/bateponto"
+            >
+              Bate-Ponto
+            </Link>
+            <Link
+              className="transition ease-in-out hover:scale-x-105 hover:text-gray-700"
+              href="/administrativo"
+            >
+              Administrativo
+            </Link>
+          </ul>
         </nav>
-    </div>
+      </div>
     </>
   );
 }
