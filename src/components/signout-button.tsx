d375d09059a53292e22d20aaf62d91856e12c960
AlostXD <junio.ribeiro02@hotmail.com>
2024-11-21 15:18:59 -0300

    Adicionando estilo aos botões de logoff

--- src/components/signout-button.tsx
+++ src/components/signout-button.tsx
@@ -7,6 +7,7 @@ export function SignOutButton() {
         "use server";
         await signOut();
       }}
+      className="bg-black p-2 rounded-lg text-white dark:bg-white dark:text-black font-extrabold transition-all hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white"
     >
       <button type="submit">Trocar de conta / sair</button>
     </form>
