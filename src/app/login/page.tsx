561d2ad88f532b47460df569785822a6edf5c337
AlostXD <junio.ribeiro02@hotmail.com>
2024-11-21 15:18:13 -0300

    Adicionando estilo aos botões de login e logoff

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,10 @@ import React from "react";
 export default function Login() {
   return (
     <>
-      <div className=""></div>
-      <SignInButton />
+      <div className="absolute inset-0 bg-[url('./img/background-login.png')] bg-cover bg-center filter blur-sm -z-10"></div>
+      <div className="flex items-center justify-center h-screen">
+        <SignInButton />
+      </div>
     </>
   );
 }
--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -7,8 +7,9 @@ export default function SignInButton() {
         "use server";
         await signIn("github", { redirectTo: "/" });
       }}
+      className="bg-black p-2 rounded-lg text-white dark:bg-white dark:text-black font-extrabold transition-all hover:bg-white hover:text-black dark:hover:bg-black dark:hover:text-white"
     >
-      <button type="submit">Signin with GitHub</button>
+      <button type="submit">Entrar com o Github</button>
     </form>
   );
 }
